bc4bd0e9f78163435231793a3fbc54aa0fad8330
RuchiTada <ruchitada@gmail.com>
2024-12-15 18:38:51 -0500

    Adding Homepage

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -15,8 +15,15 @@ const Homepage = () => {
       <div className="flex items-center justify-between"></div>
       {/* tiles */}
       <div className="">
-        <h1> </h1>
-        <p> </p>
+        <h1 className="text-gray-800 text-2xl md:text-5l lg:text-6xl font-bold">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry
+        </h1>
+        <p className="mt-8 text-md md:text-xl">
+          Lorem Ipsum has been the industry standard dummy text ever since the
+          1500s, when an unknown printer took a galley of type and scrambled it
+          to make a type specimen book.
+        </p>
       </div>
       {/* Animated button */}
       <div className=""></div>
